409d44542c85b268013b501fc774f7e4b8c2222b
Jerid Francom <francojc@wfu.edu>
2023-08-02 11:49:20 -0400

    Adds scaffold_display_questions

--- Data_Visualization/initLesson.R
+++ Data_Visualization/initLesson.R
@@ -1,4 +1,33 @@
 # Code placed in this file fill be executed every time the
-      # lesson is started. Any variables created here will show up in
-      # the user's working directory and thus be accessible to them
-      # throughout the lesson.
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+# For compatibility with 2.2.21
+.get_course_path <<- function() {
+  tryCatch(
+    swirl:::swirl_courses_dir(),
+    error = function(c) {
+      file.path(find.package("swirl"), "Courses")
+    }
+  )
+}
+
+.pathtofile <<- function(fileName) {
+  mypath <- file.path(.get_course_path(),
+                      "lessons", "Data_Visualization",
+                      fileName)
+}
+
+# @param html An html file.
+.viewer_question <<- function(html) {
+  path <- .pathtofile(html)
+  temp <- tempfile(fileext = ".html")
+  file.copy(path, temp, overwrite = TRUE)
+
+  viewer <- getOption("viewer")
+  if (!is.null(viewer))
+    viewer(temp)
+  else
+    utils::browseURL(temp)
+}

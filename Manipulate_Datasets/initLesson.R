399b8701c8d131f109176e21e4e7ec2f8e2b7df4
Jerid Francom <francojc@wfu.edu>
2023-11-03 18:39:34 -0400

    Added new files and directories for dataset manipulation and reshaping

--- Manipulate_Datasets/initLesson.R
+++ Manipulate_Datasets/initLesson.R
@@ -2,7 +2,3 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
-
-swirl_options(swirl_logging = TRUE)
-
-x <- 1:10
---
+++ Vectors/initLesson.R
@@ -0,0 +1,39 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+swirl_options(swirl_logging = TRUE)
+
+x <- 1:10
+  .get_course_path <<- function() {
+    tryCatch(
+      swirl:::swirl_courses_dir(),
+      error = function(c) {
+        file.path(find.package("swirl"), "Courses")
+      }
+    )
+  }
+
+  .pathtofile <<- function(fileName) {
+    mypath <- file.path(
+      .get_course_path(),
+      "lessons", "Variables_and_Vectors",
+      fileName
+    )
+  }
+
+  # @param html An html file.
+  .viewer_question <<- function(html) {
+    path <- .pathtofile(html)
+    temp <- tempfile(fileext = ".html")
+    file.copy(path, temp, overwrite = TRUE)
+
+    viewer <- getOption("viewer")
+    if (!is.null(viewer)) {
+      viewer(temp)
+    } else {
+      utils::browseURL(temp)
+    }
+  }
+  

29537bf955044333c856a7d7f14e07371ae5dacf
Jerid Francom <francojc@wfu.edu>
2023-12-13 17:22:41 -0500

    draft of advanced objects

--- Advanced_Objects/initLesson.R
+++ Advanced_Objects/initLesson.R
@@ -2,6 +2,7 @@
 # lesson is started. Any variables created here will show up in
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
+
 .get_course_path <<- function() {
   tryCatch(
     swirl:::swirl_courses_dir(),
@@ -32,3 +33,23 @@
     utils::browseURL(temp)
   }
 }
+
+# Variables created in this file will be accessible to the user
+
+# Create a matrix with numeric elements
+ex_int_mat <- matrix(1:6, nrow = 2, ncol = 3)
+
+# Create a matrix with character elements
+ex_char_mat <- matrix(letters[1:6], nrow = 2, ncol = 3)
+
+# Create a hypothetical data set of text messages
+ex_text_df <- tibble(
+  doc_id = c(1, 2, 3, 4, 5),
+  text = c(
+    "I am on my way. I will be there soon.",
+    "I am running late.",
+    "Are you going to be home soon?",
+    "I am running late. Maybe we should reschedule.",
+    "She is on her way."
+  )
+)

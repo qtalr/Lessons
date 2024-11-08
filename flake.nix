fec0e009ceaaab80c41d46688d0c22ca0fbbdaad
francojc <francojc@wfu.edu>
2024-11-07 21:23:46 -0500

    updates env flake

--- flake.nix
+++ flake.nix
@@ -17,9 +17,41 @@
         ];
 
         rPackages = with pkgs.rPackages; [
+          # Utils
           languageserver
           swirl
           swirlify
+          # Project
+          base64enc
+          corpora
+          dplyr
+          factoextra
+          forcats
+          fs
+          ggfortify
+          ggplot2
+          janitor
+          kableExtra
+          knitr
+          parsnip
+          purrr
+          quarto
+          readr
+          recipes
+          renv
+          rmarkdown
+          rsample
+          rstudioapi
+          scales
+          skimr
+          stringr
+          tibble
+          tidyr
+          tidytext
+          tidyverse
+          tokenizers
+          workflows
+          yardstick
         ];
 
         allPackages = packages ++ rPackages;
@@ -27,6 +59,10 @@
       {
         devShell = pkgs.mkShell {
           buildInputs = allPackages;
+          shellHook = ''
+            export R_LIBS_USER=$PWD/R/Library; mkdir -p "$R_LIBS_USER";
+            echo "R environment set up";
+          '';
         };
       });
 }
